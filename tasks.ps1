0f5239a449d2653ab502fef6a2cca841c6cc3821
Chris Kolenko <chris@webcanvas.com.au>
2015-12-25 22:40:06 +1100

    Add coveralls support

--- tasks.ps1
+++ tasks.ps1
@@ -3,7 +3,7 @@ $psake.build_success = $true
 
 Import-Properties
 Include '.\invoke-nunit3.ps1'
-Import-Tasks Clean, Test, Build, Pack-Nuspec, Tag, Push-Local, Push, Push-Tag, Version, Version-BuildServer, Set-NuGetApiKey
+Import-Tasks Clean, Test, Build, Pack-Nuspec, Tag, Push-Local, Push, Push-Tag, Version, Version-BuildServer, Set-NuGetApiKey, Send-OpenCoverResultToCoveralls
 
 Task Default -Depends Clean, Build, Test, Pack-Nuspec
 Task Release-Local -Depends Version, Clean, Build, Test, Pack-Nuspec, Push-Local -Description "Release 'ConfigLoader' to the local NuGet feed"
